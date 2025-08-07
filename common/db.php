9528bacef34dac336b5a07eb70c16b6722046d46
shiiiivaaaam <shivamkumar.official55@gmail.com>
2025-08-07 11:46:01 +0530

    Update db.php

--- common/db.php
+++ common/db.php
@@ -1,11 +1,12 @@
 <?php
     $host= 'localhost';
-    $username= 'root';
-    $password= null;
+    $username= ''; // write username here 
+    $password= ; // passwrod here .
     $dbname= 'discuss';
 
     $conn = new mysqli($host,$username,$password,$dbname);
     if ($conn->connect_error) {
         die('database connection error'. $conn->connect_error);
     }
+
 ?>
